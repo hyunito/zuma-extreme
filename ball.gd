29a689a0b3f7eaf47806a58c7efcbb22284a7414
hyunito <eujengonzales@gmail.com>
2026-05-25 13:55:57 +0800

    first commit

---
+++ ball.gd
@@ -0,0 +1,34 @@
+extends PathFollow2D
+
+@export var speed: float = 150.0
+
+const BALL_TEXTURES = {
+	"red": preload("res://assets/balls/Red_ball.png"),
+	"blue": preload("res://assets/balls/Blue_ball.png"),
+	"yellow": preload("res://assets/balls/Yellow_ball.png")
+}
+
+var ball_color: String = "red"
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+func _physics_process(delta: float) -> void:
+	progress += speed * delta
+	
+	if sprite:
+		sprite.rotation = progress / 20.0
+
+	if progress_ratio >= 1.0:
+		reach_end()
+
+func set_color(new_color: String) -> void:
+	ball_color = new_color
+	
+	if not is_node_ready():
+		await ready
+		
+	if sprite and BALL_TEXTURES.has(new_color):
+		sprite.texture = BALL_TEXTURES[new_color]
+
+func reach_end() -> void:
+	queue_free()
---
+++ track.gd
@@ -0,0 +1,30 @@
+extends Path2D
+
+const BALL_SCENE = preload("res://ball.tscn")
+
+@onready var line_2d: Line2D = $Line2D
+
+var spawn_timer: float = 0.0
+var spawn_interval: float = 1.0
+
+const COLORS = ["red", "blue", "yellow"]
+
+func _ready() -> void:
+	if line_2d and curve:
+		line_2d.points = curve.get_baked_points()
+		
+func _process(delta: float) -> void:
+	spawn_timer += delta
+	if spawn_timer >= spawn_interval:
+		spawn_timer = 0.0
+		spawn_ball()
+		
+func spawn_ball() -> void:
+	var new_ball = BALL_SCENE.instantiate()
+	
+	add_child(new_ball)
+	new_ball.progress = 0.0
+	
+	var random_color = COLORS[randi() % COLORS.size()]
+	new_ball.set_color(random_color)
+	
