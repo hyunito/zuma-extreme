0429f8e7d1cb995cf57ea765a46235fa0ca542d2
hyunito <eujengonzales@gmail.com>
2026-05-28 20:40:48 +0800

    Bug fixes

--- player.gd
+++ player.gd
@@ -4,11 +4,19 @@ var bullet_scene = preload("res://shot_ball.tscn")
 @onready var mouth_position = $Marker2D
 @onready var loaded_ball_sprite = $LoadedBall
 
-var possible_textures = [
-	preload("res://assets/balls/Red_ball.png"),
-	preload("res://assets/balls/Blue_ball.png"),
-	preload("res://assets/balls/Yellow_ball.png")
-]
+# 1. Map color names (strings) to their textures
+const BALL_TEXTURES = {
+	"red": preload("res://assets/balls/Red_ball.png"),
+	"blue": preload("res://assets/balls/Blue_ball.png"),
+	"yellow": preload("res://assets/balls/Yellow_ball.png")
+}
+
+# 2. Store the current loaded color name
+var current_color: String = "red"
+
+func _ready() -> void:
+	# Randomly load the first ball at startup
+	reload()
 
 func _process(_delta):
 	look_at(get_global_mouse_position())
@@ -21,16 +29,21 @@ func shoot():
 	# 1. Create the bullet
 	var new_bullet = bullet_scene.instantiate()
 	
-	# 2. MATCH THE COLORS! We force the bullet to use the exact same image as our Dummy ball.
-	# (We use get_node to find the Sprite2D inside the newly spawned bullet)
-	new_bullet.get_node("Sprite2D").texture = loaded_ball_sprite.texture
+	# 2. Assign the color string AND texture to the bullet!
+	new_bullet.ball_color = current_color
+	new_bullet.get_node("Sprite2D").texture = BALL_TEXTURES[current_color]
 	
 	# 3. Position and fire!
 	new_bullet.global_position = mouth_position.global_position
 	new_bullet.rotation = rotation
-	# ADD THIS LINE: Copy the player's active scale to the bullet!
 	new_bullet.scale = global_scale
 	get_parent().add_child(new_bullet)
 	
-	# 4. Reload the mouth with a brand new random color for the next shot!
-	loaded_ball_sprite.texture = possible_textures.pick_random()
+	# 4. Reload the mouth
+	reload()
+
+func reload() -> void:
+	# Pick a random color string from our dictionary keys
+	current_color = BALL_TEXTURES.keys().pick_random()
+	# Apply the corresponding texture
+	loaded_ball_sprite.texture = BALL_TEXTURES[current_color]
--- shot_ball.gd
+++ shot_ball.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var speed: float = 2000.0
 var animation_timer: float = 0.0
 var frame_rate: float = 0.05 
+var ball_color: String = "red"
 
 func _physics_process(delta: float) -> void:
 	var motion = transform.x * speed * delta
