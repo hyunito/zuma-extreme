27fea53e9965611cdda3cc0c73fae54e84914ca0
cyrusjcs <silvorcyrus1234@gmail.com>
2026-06-02 07:41:31 +0800

    Fixed Game Over Issues
    Fixed the part where even if it's game over, you can click anywhere and sound effects can still be heard

--- player.gd
+++ player.gd
@@ -28,6 +28,8 @@ func _process(_delta):
 	look_at(get_global_mouse_position())
 
 func _unhandled_input(event):
+	if get_tree().paused:
+		return
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		shoot()
 
